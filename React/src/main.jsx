510c0a4a1cfccf48ee2fabda6e278ddff52c908f
Mustafa Faraz <mustafafaraz87@gmail.com>
2025-07-06 12:24:07 +0000

    first react app

---
+++ React/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import { Fragment } from "react";
+import "./index.css";
+import App from "./App.jsx";
+
+const root = document.getElementById("root");
+
+createRoot(root).render(<App />);
